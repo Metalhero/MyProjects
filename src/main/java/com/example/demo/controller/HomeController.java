0da91ea9d5400207daa2656f2874c6a8aa6328fd
Metalhero <sl3yerkem@gmail.com>
2021-04-16 10:47:37 +0200

    4

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,12 +1,7 @@
-package io.portfolio.application.controller;
+package com.example.demo.controller;
 
-import io.portfolio.application.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-
-import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class HomeController {
